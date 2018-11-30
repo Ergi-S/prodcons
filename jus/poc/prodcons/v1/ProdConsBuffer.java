3c5d86c6ec8ec6e59d59497ab95d234caa29b1ae
Unknown <solverytom@hotmail.fr>
2018-11-30 09:26:53 +0100

    ajout class Buffer

---
+++ jus/poc/prodcons/v1/ProdConsBuffer.java
@@ -0,0 +1,24 @@
+package jus.poc.prodcons.v1;
+
+public class ProdConsBuffer  implements IProdConsBuffer{
+
+	@Override
+	public void put(Message m) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Message get() throws InterruptedException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public int nmsg() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
